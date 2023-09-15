a9e7dbe35597bf7e041509e871eb2e7628b3b73a
taehwan1-6 <47453097+taehwan1-6@users.noreply.github.com>
2023-09-16 02:09:32 +0900

    refactor: MemberVO 수정
    - 회원 테이블의 컬럼 수정으로 VO 수정

--- src/main/java/kr/ac/kopo/hanabit/member/v1/vo/MemberVO.java
+++ src/main/java/kr/ac/kopo/hanabit/member/v1/vo/MemberVO.java
@@ -5,15 +5,14 @@ import lombok.Getter;
 @Getter
 public class MemberVO {
 
-    private Long id;
-    private String useId;
-    private String password;
-    private String residentRegistrationNumber;
-    private String name;
-    private String phone;
-    private String email;
-    private String address;
-    private String createdAt;
-    private String profileImage;
+	private String useId;
+	private String password;
+	private String name;
+	private String phone;
+	private String birth;
+	private String email;
+	private String address;
+	private String createdAt;
+	private String profileImage;
 
 }
